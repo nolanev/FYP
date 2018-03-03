0d3784558eea6dd8a29ecaa0154bd732e2cd1cf7
nolanev <nolanev@tcd.ie>
2018-03-03 16:20:58 +0000

    overlay done

---
+++ main.js
@@ -0,0 +1,82 @@
+
+
+
+var myWorkspace = Blockly.inject('blocklyDiv',
+      {media: 'blockly/media/', 
+	  toolbox: document.getElementById('toolbox'),
+     zoom:
+         {controls: true,
+          wheel: true,
+          startScale: 1.0,
+          maxScale: 3,
+          minScale: 0.3,
+          scaleSpeed: 1.2},
+     trashcan: true});
+  
+	 
+	 function showCodeJavaScript() {
+      // Generate JavaScript code and display it.
+      Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+      var code = Blockly.JavaScript.workspaceToCode(myWorkspace);
+      alert(code);
+    }
+	function overlayOn(colour){
+		var choice;
+		switch(colour){
+			case 1:
+				choice="pinkOverlay";
+				break;
+				
+			case 2:
+				choice="yellowOverlay";
+				break;
+			case 3:
+				choice="blueOverlay";
+				break;
+		}
+		if (document.getElementById(choice).style.display == "block"){
+			document.getElementById(choice).style.display = "none";
+		}
+		else{
+		document.getElementById(choice).style.display = "block";
+		}
+		
+	}
+	
+
+
+
+
+
+function overlayMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+    function runCodeJavaScript() {
+      // Generate JavaScript code and run it.
+      window.LoopTrap = 1000;
+      Blockly.JavaScript.INFINITE_LOOP_TRAP =
+          'if (--window.LoopTrap == 0) throw "Infinite loop.";\n';
+	  Blockly.JavaScript.addReservedWords('code');
+      var code = Blockly.JavaScript.workspaceToCode(myWorkspace);
+      Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+      try {
+        eval(code);
+      } catch (e) {
+        alert(e);
+      }
+    }
+	  
